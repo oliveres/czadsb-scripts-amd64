f792610075c7ca09ababc24a737f2449b4c29c6e
oliveres <31700240+oliveres@users.noreply.github.com>
2021-01-27 12:39:55 +0100

    Update mm2_install.sh

--- mm2_install.sh
+++ mm2_install.sh
@@ -8,8 +8,8 @@ username="$(id -u -n)"
 mkdir ~/modesmixer2
 cd ~/downloads
 
-wget http://freedom.dicea.unifi.it/debian/pool/main/o/openssl/libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
-sudo dpkg -i libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
+wget http://mirrors.mediatemple.net/debian-archive/debian/pool/main/o/openssl/libssl1.0.0_1.0.2l-1~bpo8+1_amd64.deb
+sudo dpkg -i libssl1.0.0_1.0.2l-1~bpo8+1_amd64.deb
 
 # Use device specific MM2 - 32bit on RPI or Orange H5 64bit, there are no other choices http://xdeco.org/?page_id=30
 if cat /proc/device-tree/model | grep -q 'Raspberry Pi 4'; then
@@ -20,7 +20,7 @@ elif cat /proc/device-tree/model | grep -q 'Raspberry'; then
     cp ~/czadsb-scripts/mm2/modesmixer2-rpi2-3_deb9 ~/modesmixer2/modesmixer2
 elif [ `getconf LONG_BIT` = "64" ]; then
     echo "64 bit system detected, using AllWinner H5 64bit version"
-    cp ~/czadsb-scripts/modesmixer2-64 ~/modesmixer2/modesmixer2
+    cp ~/czadsb-scripts/modesmixer2 ~/modesmixer2/modesmixer2
 else
     echo "Unable to found right version of MM2, using default one, its possible that it will not work" 2>&1
     cp ~/czadsb-scripts/mm2/modesmixer2-rpi2-3_deb9 ~/modesmixer2/modesmixer2
