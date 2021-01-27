a4192990d591d54880b8693579a19a614ff7c48c
oliveres <31700240+oliveres@users.noreply.github.com>
2021-01-27 16:02:35 +0100

    Update autostart.sh

--- starters/autostart.sh
+++ starters/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
   username="$(id -u -n)"
+  sudo sed -i -e '#!/bin/sh\n' /etc/rc.local
   sudo sed -i -e '$i sudo /home/'$username'/mlat-client/czadsb.mlat-client.sh &\n' /etc/rc.local
   sudo sed -i -e '$i sudo /home/'$username'/n2n/czadsb.n2n.sh &\n' /etc/rc.local
   sed -i "s/USERNAME/$username/g" /home/$username/modesmixer2/czadsb.modesmixer.sh
   sudo sed -i -e '$i sudo /home/'$username'/modesmixer2/czadsb.modesmixer.sh &\n' /etc/rc.local
+  sudo sed -i -e 'exit 0' /etc/rc.local
